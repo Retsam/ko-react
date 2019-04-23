a18f9e61eeb1687baef7f866d18c4cccc03f0999
Retsam <chess19@gmail.com>
2019-04-23 18:01:45 -0400

    Rework useComputed, change default behavior for deps array

--- src/hooks/useComputed.ts
+++ src/hooks/useComputed.ts
@@ -1,5 +1,6 @@
 import ko from "knockout";
-import { useState, useLayoutEffect, useEffect } from "react";
+import useForceUpdate from "hooks/utils/useForceUpdate";
+import useMemoWithDisposer from "hooks/utils/useMemoWithDisposer";
 
 /**
  * Returns the result of a provided function, causing a rerender whenever
@@ -7,28 +8,18 @@ import { useState, useLayoutEffect, useEffect } from "react";
  * @param func A pure function that reads observables to produce a value,
  *      (does not need to be a ko.computed, and probably shouldn't be)
  */
-function useComputed<T>(func: () => T, deps: any[] = []) {
-    // The func is put in an observable, so that it can be changed
-    //  whenever dependencies (e.g. closure variables used by func) change
-    const [ funcObservable ] = useState(() => ko.observable(func));
-    const [ computed ] = useState(() => ko.pureComputed(() => (
-        funcObservable()()
-    )));
-    const [ value, setValue ] = useState(computed.peek());
-
-    // Doing useLayoutEffect so that the subscription happens synchronously with the initial render;
-    // eliminates a window in which the component and computed can go out of sync
-    useLayoutEffect(() => {
-        computed.subscribe(val => setValue(val));
-        return () => computed.dispose();
-    }, []);
-
-    // When deps change, replace the function
-    useEffect(() => {
-        funcObservable(func);
-    }, deps);
-
-    return value;
+function useComputed<T>(func: () => T, deps?: any[]) {
+    const forceUpdate = useForceUpdate();
+    const computed = useMemoWithDisposer(
+        () => {
+            const c = ko.pureComputed(func);
+            c.subscribe(forceUpdate);
+            return c;
+        },
+        (computed) => computed.dispose(),
+        deps,
+    );
+    return computed();
 }
 
 export default useComputed;
--- tests/hooks/useComputed.test.tsx
+++ tests/hooks/useComputed.test.tsx
@@ -32,7 +32,7 @@ test("doesn't call the render or computed function unnecessarily", () => {
             return (
             <div>{c()}</div>
             );
-        });
+        }, []);
     };
     const count = ko.observable(0);
     mount(<Component c={count} />);
@@ -43,20 +43,53 @@ test("doesn't call the render or computed function unnecessarily", () => {
     });
     expect(renderCount).toBe(2);
     expect(computedCount).toBe(2);
-
 });
 
-test("can be used with non-observable state" , () => {
+test("can be used with closure values" , () => {
     const Counter = () => {
         const [count, setCount] = useState(0);
+        // NOT passing a dependency array: behaves correctly, but may compute more often than necessary
         return useComputed(() => (
             <div onClick={() => setCount(count + 1)}>Value is {count}</div>
-        ), [count]);
+        ));
     };
     const element = mount(<Counter />);
     act(() => {
         element.simulate('click');
     });
     expect(element.text()).toBe("Value is 1");
+});
 
-})
+test("doesn't call the render or computed function unnecessarily with deps", () => {
+    let renderCount = 0;
+    let computedCount = 0;
+    const Counter = ({plus}: {plus: KnockoutObservable<number>}) => {
+        renderCount++;
+        const [count, setCount] = useState(0);
+        return useComputed(() => (
+            computedCount++,
+            <div onClick={() => setCount(count + 1)}>Value is {count + plus()}</div>
+        ), [count]);
+    };
+    const plus = ko.observable(0);
+    const element = mount(<Counter plus={plus} />);
+    expect(renderCount).toBe(1);
+    expect(computedCount).toBe(1);
+    expect(element.text()).toBe("Value is 0");
+
+    // Update observable state
+    act(() => {
+        plus(2);
+    });
+    expect(renderCount).toBe(2);
+    expect(computedCount).toBe(2);
+    expect(element.text()).toBe("Value is 2");
+
+    // Update non-observable state
+    act(() => {
+        element.simulate('click');
+    });
+    expect(renderCount).toBe(3);
+    expect(computedCount).toBe(3);
+    expect(element.text()).toBe("Value is 3");
+});
