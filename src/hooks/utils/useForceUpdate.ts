0f7cea2d4214bd809a41180e8ed9a2af5e2ad8a0
Retsam <chess19@gmail.com>
2019-07-09 11:05:11 -0400

    Write useObservable in terms of useSubscription

--- src/hooks/utils/useForceUpdate.ts
+++ src/hooks/utils/useForceUpdate.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 /** Returns a function that can be called to force the component to rerender */
 export default function useForceUpdate() {
-    const [/*val*/, setIncr] = useState(0);
-    // Using callback form of setIncr so that the same useForceUpdate function can be called multiple times
-    return () => setIncr((val) => val + 1);
+    const [/*val*/, setVal] = useState(0);
+    // Using callback form of setVal so that the same useForceUpdate function can be called multiple times
+    return () => setVal(val => val + 1);
 }
