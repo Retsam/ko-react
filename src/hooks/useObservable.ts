9deac8a27f5f2f8bc98590d4ae4fc7750d306106
Retsam <chess19@gmail.com>
2022-05-17 15:49:03 -0400

    Log errors from useObservable

--- src/hooks/useObservable.ts
+++ src/hooks/useObservable.ts
@@ -12,9 +12,16 @@ export type ReadonlyObservable<T> = Pick<KnockoutObservable<T>, "subscribe" | "p
 function useObservable<T>(observable: ReadonlyObservable<T>) {
     useSubscription(observable, useForceUpdate());
 
-    // Passing undocument `true` option to `observable.peek` to force it to read the latest value
-    // from the observable, even if it's been rate-limited.
-    return (observable.peek as any)(true) as T;
+    try {
+        // Passing undocument `true` option to `observable.peek` to force it to read the latest value
+        // from the observable, even if it's been rate-limited.
+        return (observable.peek as any)(true) as T;
+    } catch(e) {
+        // The errors seem to get swallowed somewhere in the React machinery, so log them here
+        // tslint:disable-next-line
+        console.error("An error occurred while reading an observable", e);
+        throw e;
+    }
 }
 
 export default useObservable;
