aacacb0219caed0ab13a0a2454e2a66ec3eaa5ed
Retsam <chess19@gmail.com>
2025-02-24 21:35:54 -0500

    Use @swc/jest

--- src/reactToKnockout/KnockoutTemplate.tsx
+++ src/reactToKnockout/KnockoutTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import useKnockoutBindings from "reactToKnockout/useKnockoutBindings";
 
 export interface KnockoutTemplateProps {
